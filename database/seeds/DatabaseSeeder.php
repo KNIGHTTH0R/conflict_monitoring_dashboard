f3f1dc5bf6808aa38c069e3609dade642d99a849
waiyan112 <waiyan112@gmail.com>
2017-03-28 14:58:41 +0630

    update default seeding data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\Category::class,20)->create();
+        factory(App\Facebook_page::class,20)->create();
+        factory(App\Website::class,20)->create();
+        factory(App\User::class,20)->create();
+        factory(App\Monitor::class,20)->create();
     }
 }
