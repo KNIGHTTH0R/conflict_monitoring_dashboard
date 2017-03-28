16ff0a8aad7622c55be76d98400301aa1835d7e9
waiyan112 <waiyan112@gmail.com>
2017-03-28 14:58:18 +0630

    fix monitr table and add new 4 tables

--- database/migrations/2017_03_21_081657_create_new_monitor.php
+++ database/migrations/2017_03_21_081657_create_new_monitor.php
@@ -21,11 +21,27 @@ class CreateNewMonitor extends Migration
             $table->date('from_time');
             $table->date('to_time');
             $table->text('keywords');
-            $table->integer('page_id');
-            $table->integer('website_id');
-            $table->integer('special_page_id');
-            $table->integer('special_website_id');
             $table->timestamps();
+        });
+
+        Schema::create("monitors_pages_relations",function (Blueprint $table){
+            $table->integer("monitor_id");
+            $table->integer("page_id");
+        });        
+
+        Schema::create("monitors_websites_relations",function (Blueprint $table){
+            $table->integer("monitor_id");
+            $table->integer("website_id");
+        });        
+
+        Schema::create("monitors_special_pages_relations",function (Blueprint $table){
+            $table->integer("monitor_id");
+            $table->integer("page_id");
+        });        
+
+        Schema::create("monitors_special_websites_relations",function (Blueprint $table){
+            $table->integer("monitor_id");
+            $table->integer("website_id");
         });
 
         Schema::create('facebook_pages', function (Blueprint $table) {
@@ -61,5 +77,9 @@ class CreateNewMonitor extends Migration
         Schema::dropIfExists('facebook_pages');
         Schema::dropIfExists('websites');
         Schema::dropIfExists('categories');
+        Schema::dropIfExists('monitors_pages_relations');
+        Schema::dropIfExists('monitors_websites_relations');
+        Schema::dropIfExists('monitors_special_pages_relations');
+        Schema::dropIfExists('monitors_special_websites_relations');
     }
 }
